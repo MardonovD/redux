496d82e52b0ab892be8503e4259fb4082dd44a73
Dostonbek <mardonovdoston69@gmail.com>
2022-05-04 18:17:08 +0300

    render

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-import './styles.css'
+import "./styles.css";
+
+const counter = document.querySelector("#counter");
+const addBtn = document.getElementById("add");
+const sunBtn = document.querySelector("#sub");
+
+let state = 0;
+function render() {
+  counter.textContent = state.toString();
+}
+
+render();
+
+addBtn.addEventListener("click", () => {
+  state++;
+  render();
+});
+
+sunBtn.addEventListener("click", () => {
+  state--;
+  render();
+});
