4db52cba67a280c536608e3df47994d8cfc4633e
Dostonbek <mardonovdoston69@gmail.com>
2022-05-04 18:20:21 +0300

    btn submit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import "./styles.css";
 const counter = document.querySelector("#counter");
 const addBtn = document.getElementById("add");
 const sunBtn = document.querySelector("#sub");
+const asyncBtn = document.getElementById("async");
 
 let state = 0;
 function render() {
   counter.textContent = state.toString();
 }
-
-render();
 
 addBtn.addEventListener("click", () => {
   state++;
@@ -20,3 +19,12 @@ sunBtn.addEventListener("click", () => {
   state--;
   render();
 });
+
+asyncBtn.addEventListener("click", () => {
+  setTimeout(() => {
+    state++;
+    render();
+  }, 2000);
+});
+
+render();
